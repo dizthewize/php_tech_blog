b58ab116235389e5d37545edc27c2204a75d37ab
Unknown <martez_conner@outlook.com>
2017-11-04 10:20:30 -0700

    config

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-if(env('APP_ENV') === 'production') {
-  URL::forceSchema('https');
-}
-
 Route::resource('posts', 'PostsController');
 Route::resource('comments', 'CommentsController', ['except' => 'index']);
 
