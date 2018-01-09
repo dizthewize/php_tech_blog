31c4f8c6496a0a532ee091367b8c444ca0b15693
Unknown <martez_conner@outlook.com>
2018-01-08 19:52:38 -0800

    Changed footer link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,5 +4,5 @@
         <li><a href="https://github.com/martezconner"><i class="fa fa-github" aria-hidden="true"></i></a></li>
         <li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
     </ul>
-    <p class="text-muted">&copy; A project by <span><a href="#">Martez Conner</a></span></p>
+    <p class="text-muted">&copy; A project by <span><a href="https://martezconner.herokuapp.com">Martez Conner</a></span></p>
 </footer>
