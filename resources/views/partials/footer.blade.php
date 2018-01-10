09b40f830024dd4b667c1e2d445a6943a7937aca
Unknown <martez_conner@outlook.com>
2018-01-09 18:40:38 -0800

    Finished deployment to AWS

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="text-center">
     <ul class="list-inline">
-        <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+        <li><a href="https://www.linkedin.com/in/martez-conner-b38038108/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
         <li><a href="https://github.com/martezconner"><i class="fa fa-github" aria-hidden="true"></i></a></li>
-        <li><a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+        <li><a href="https://dribbble.com/martezconner"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
     </ul>
     <p class="text-muted">&copy; A project by <span><a href="https://martezconner.herokuapp.com">Martez Conner</a></span></p>
 </footer>
